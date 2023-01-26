69d07633c82583cda1fb470b8537e2e21491b334
Santiago Nicolás Sanchez <nicosanchez9395@gmail.com>
2023-01-26 04:01:19 -0300

    pre-entrega1

---
+++ carpetas/index.js
@@ -0,0 +1,48 @@
+/*  >< */
+
+/* let nombre = prompt("ingrese su nombre");
+if (nombre === "nicolas" || nombre.toLowerCase === "nicolas") {
+  alert("es un cra");
+} else {
+  alert("no so un cra");
+} */
+
+/*
+let nombre = prompt("ingrese su nombre");
+if (nombre.toLowerCase() === "nicolas") {
+  alert("es un cra");
+} else {
+  alert("no so un cra");
+} 
+ */
+
+/*
+ for (let i = 0; i < 10; i++) {
+    let resultado = 
+}
+*/
+
+let acertijo = prompt("1+1 es igual a 2?");
+if (acertijo === "si") {
+  alert("Respuesta correcta");
+} else {
+  alert("INCORRECTO!");
+}
+let historia = prompt("Argentina se independizo en 1810?");
+if (historia === "si") {
+  alert("Respuesta correcta");
+} else {
+  alert("INCORRECTO!");
+}
+let deportes = prompt("Messi es el numero 1?");
+if (deportes === "si") {
+  alert("Respuesta correcta");
+} else {
+  alert("INCORRECTO!");
+}
+
+
+
+if (acertijo + historia + deportes === true){
+  alert ("Enhorabuena, acertaste a todas las preguntas. Tienes un optimo!")
+}else("has fallado, desapruebas este examen")
